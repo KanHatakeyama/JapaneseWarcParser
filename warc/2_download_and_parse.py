fc1632f0a5ca7c20fb8200c852c12ec17f43a19d
MiuraTOMO <7217074@alumni.tus.ac.jp>
2024-02-24 18:23:30 +0000

    [FIX]clean code

--- warc/2_download_and_parse.py
+++ warc/2_download_and_parse.py
@@ -1,45 +1,34 @@
-from src.cleaner.LineChecker import remove_dup_lines, remove_multi_headers
-from datasets import load_dataset
+"""
+Based on the path list downloaded from CommonCrawl
+
+1) Download warc.gz path, convert to warc file
+
+2) Load the warc file and search for only Japanese sites,
+  Extract information from that site
+
+3) Combine the text of each tag
+
+4) Annotate usable and unusable texts based on fasttext
+"""
 import sys
+import random
+import argparse
+sys.path.append("../")
+
+from datasets import load_dataset
+
+from src.cleaner.LineChecker import remove_dup_lines, remove_multi_headers
 from src.cleaner.WordIntegrator import WordIntegrator
 from src.cleaner.PerplexityChecker import PerplexityChecker
 from src.file_utils import make_dir
 from src.parse_warc import extract_japanese_from_warc
 from src.downloader import get_cc_path_list, download_warc_file
-import random
-sys.path.append("../")
-
-"""
-download and parse warc file
-this script can be done in parallel
-
-"""
-
 if True:
     from mc4s.src.classifier.DatasetAnnotator import DatasetAnnotator
     from mc4s.src.cleaner.auto_cleaner import clean_text
 
-# tagで分割されたテキストを統合するためのintegrator
-integrator = WordIntegrator(
-    checker=PerplexityChecker("data/lm_sp/ja.arpa.bin",
-                              "data/lm_sp/ja.sp.model",
-                              ),
-    filter_path="dict/header_filter.txt",
-    start_filter_path="dict/start_filter.txt",
-    end_filter_path="dict/end_filter.txt"
-)
 
-# テキストを分類するためのannotator
-dataset = load_dataset('mc4', 'ja', split='train', streaming=True)
-annotator = DatasetAnnotator(dataset, clean_func=clean_text, n_preload=500,
-                             out_path="../mc4s/annotations"
-                             )
-
-corpus_dir = "data/corpus"
-make_dir(corpus_dir)
-
-
-def download_and_parse(cc_path):
+def download_and_parse(cc_path, is_clean=False):
     # download warc file
     warc_path = download_warc_file(cc_path)
 
@@ -47,38 +36,92 @@ def download_and_parse(cc_path):
     print("parsing "+warc_path)
     try:
         tag_records = extract_japanese_from_warc(warc_path)
+        if is_clean:
+            # clean
+            cleaned_records = []
+            for id in range(len(tag_records)):
+                record = tag_records[id]
+                lines = [i[0].strip() for i in record["text"]]
+                lines = integrator(lines)
+                txt = "\n".join(lines)
+                if txt != "":
+                    cleaned_records.append(
+                        {"url": record["url"], "timestamp": record["timestamp"], "text": txt})
+
+            # classify and save
+            for record in cleaned_records:
+                is_noise = annotator.predict(record["text"])
+                if is_noise == 1:
+                    continue
+                filename = warc_path.split("/")[-1].replace(".warc", "")
+                save_path = f"{corpus_dir}/{filename}.txt"
+                with open(save_path, "a") as f:
+                    f.write(str(record)+"\n")
+            print("done "+warc_path)
+        else:
+            save_dict = {
+                "tag_records" : tag_records,
+                "cc_path" : cc_path,
+                "warc_path" : warc_path
+            }
+            file_name = os.path.basename(warc_path)
+            base_name = os.path.splitext(file_name)[0]
+            file_base_name = "_".join(base_name.split("_")[2:])
+            save_gz_path = f"/tmp/{file_base_name}_japanese.json.gz"
+            with gzip.open(save_gz_path, 'wt', encoding="ascii") as zipfile:
+               json.dump(save_dict, zipfile)
+        # clear warc file
+        with open(warc_path, "w") as f:
+            f.write("")
     except Exception as e:
         print("fail loading "+warc_path)
-
-    # clean
-    cleaned_records = []
-    for id in range(len(tag_records)):
-        record = tag_records[id]
-        lines = [i[0].strip() for i in record["text"]]
-        lines = integrator(lines)
-        txt = "\n".join(lines)
-        if txt != "":
-            cleaned_records.append(
-                {"url": record["url"], "timestamp": record["timestamp"], "text": txt})
-
-    # classify and save
-    for record in cleaned_records:
-        is_noise = annotator.predict(record["text"])
-        if is_noise == 1:
-            continue
-        filename = warc_path.split("/")[-1].replace(".warc", "")
-        save_path = f"{corpus_dir}/{filename}.txt"
-        with open(save_path, "a") as f:
-            f.write(str(record)+"\n")
-
-    print("done "+warc_path)
-    # clear warc file
-    with open(warc_path, "w") as f:
-        f.write("")
+        return 
 
 
-# get path list
-cc_path_list = get_cc_path_list()
-random.shuffle(cc_path_list)
-for cc_path in cc_path_list:
-    download_and_parse(cc_path)
+def main():
+    """
+    Parameter 
+    -------------
+    seed :
+        Path list shuffle seed
+    is_clean : 
+        Flag for whether to perform data cleaning
+    batch_number :
+        Identification number when processing the path list by dividing it
+        if batch_number = -1, run all paths 
+    """
+    # argparse
+    parser = argparse.ArgumentParser(
+            "Download/unzip/process the Japanese language of the downloaded commoncrawl gz path")
+    parser.add_argument("seed", default=42, type=int, help="Path list shuffle seed")
+    parser.add_argument("is_clean", type=bool, default=True help="Flag for whether to perform data cleaning")
+    parser.add_argument("batch_nubmer", default=-1, type=int, default=True 
+                        help="Identification number when processing the path list by dividing it")
+    args = parser.parse_args()
+
+    # integrator for integrating text split by tag
+    integrator = WordIntegrator(
+        checker=PerplexityChecker("data/lm_sp/ja.arpa.bin",
+                                  "data/lm_sp/ja.sp.model",
+                                  ),
+        filter_path="dict/header_filter.txt",
+        start_filter_path="dict/start_filter.txt",
+        end_filter_path="dict/end_filter.txt"
+    )
+    # annotator for classifying text
+    dataset = load_dataset('mc4', 'ja', split='train', streaming=True)
+    annotator = DatasetAnnotator(dataset, clean_func=clean_text, n_preload=500,
+                                 out_path="../mc4s/annotations"
+                                 )
+    corpus_dir = "data/corpus"
+    make_dir(corpus_dir)
+
+    # get path list
+    random.seed(args.seed)
+    cc_path_list = get_cc_path_list()
+    random.shuffle(cc_path_list)
+    for cc_path in cc_path_list:
+        download_and_parse(cc_path, is_clean=args.is_clean)
+
+if __name__ == "__main__":
+    main()
